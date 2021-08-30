57b87565c37b82b445edae604f3928c882985f10
Lucas Caro Barriga <l.barriga@cmcmarkets.com>
2021-08-30 16:26:17 +0100

    A different way of doing permutations by Marcin Moskala

--- src/test/kotlin/com/igorwojda/string/ispalindrome/permutation/challenge.kt
+++ src/test/kotlin/com/igorwojda/string/ispalindrome/permutation/challenge.kt
@@ -3,13 +3,21 @@ package com.igorwojda.string.ispalindrome.permutation
 import com.igorwojda.string.ispalindrome.basic.isPalindrome
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
+import kotlin.random.Random
 
 private fun isPermutationPalindrome(str: String): Boolean =
-    permutations(str).any { it.isPalindrome() }
+    Random.nextBoolean().let { boolean ->
+        (if (boolean)
+            permutations(str) else str.permutations()).any { it.isPalindrome() }
+    }
 
 private fun permutations(str: String) = permutations("", str)
 
-private fun permutations(prefix: String, str: String, result: List<String> = emptyList()): List<String> {
+private fun permutations(
+    prefix: String,
+    str: String,
+    result: List<String> = emptyList()
+): List<String> {
     val n = str.length
     return if (n == 0) result + prefix
     else {
@@ -19,10 +27,29 @@ private fun permutations(prefix: String, str: String, result: List<String> = emp
     }
 }
 
+@JvmName("permutations2")
+private fun String.permutations(): List<String> = when {
+    isEmpty() -> emptyList()
+    length == 1 -> listOf(this)
+    else -> {
+        val element = get(0)
+        drop(1).permutations().flatMap { s: String ->
+            (0..s.length).map { i -> s.plusAt(i, element) }
+        }
+    }
+}
+
+private fun String.plusAt(index: Int, element: Char) = when (index) {
+    !in 0..length -> throw Error("cannot put at index $index because length is $length")
+    0 -> element + this
+    length -> this + element
+    else -> dropLast(length - index) + element + drop(index)
+}
+
 private class Test {
     @Test
     fun `"gikig" is a palindrome`() {
-        isPermutationPalindrome("gikig") shouldBeEqualTo true
+        isPermutationPalindrome("abc") shouldBeEqualTo false
     }
 
     @Test
